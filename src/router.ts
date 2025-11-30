4b700949e7bcdfc8f23a8136304deca8ccb29a00
Vitor Linaris Notebook <vitor.linaris@gmail.com>
2025-11-30 13:27:45 -0300

    feat: add router

---
+++ src/router.ts
@@ -0,0 +1,35 @@
+import path from 'node:path';
+import { Router } from 'express';
+
+//import { listProducts } from './app/useCases/Products/listProducts';
+//import { createProducts } from './app/useCases/Products/createProducts';
+//import { listOrders } from './app/useCases/orders/listOrders';
+//import { createOrder } from './app/useCases/orders/createOrder';
+//import { cancelOrder } from './app/useCases/orders/cancelOrder';
+
+export const router = Router();
+
+// list categories
+//router.get('/categories', listCategories);
+
+// create category
+// router.post('/categories', createCategory);
+
+/* // list products
+router.get('/products', listProducts);
+
+// create product
+router.post('/products' , createProducts);
+
+// Get product by category
+router.get('/categories/:categoryId/products', listProductsByCategory);
+
+// list orders
+router.get('/orders', listOrders);
+
+// create order
+router.post('/orders', createOrder);
+
+// delete order
+router.delete('/orders/:orderId', cancelOrder); */
+
